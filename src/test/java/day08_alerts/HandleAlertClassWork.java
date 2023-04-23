5b1d753f816078b2ef56e91dd506d537161f56cd
muzafferugur <av.muzafferugur@gmail.com>
2023-04-24 00:00:02 +0300

    23.04.2023

---
+++ src/test/java/day08_alerts/HandleAlertClassWork.java
@@ -0,0 +1,12 @@
+package day08_alerts;
+
+public class HandleAlertClassWork {
+
+
+    /*
+    - https://the-internet.herokuapp.com/javascript_alerts adresine gidelim
+    - "Click for JS Alert" butonuna tıklayalım
+    - Tıkladıktan sonra çıkan uyarı mesajına (alerte) tamam diyelim
+     */
+
+}
