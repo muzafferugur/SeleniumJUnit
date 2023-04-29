ee7ed1063a56e56742be28cebc66d2e67c85ebd1
muzafferugur <av.muzafferugur@gmail.com>
2023-04-29 19:42:24 +0300

    29.04.2023

--- src/test/java/day11_Faker_File/C02_KeyboardActions.java
+++ src/test/java/day11_Faker_File/C02_KeyboardActions.java
@@ -1,6 +1,11 @@
 package day11_Faker_File;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class C02_KeyboardActions extends TestBase {
@@ -13,8 +18,24 @@ public class C02_KeyboardActions extends TestBase {
         4.videoyu çalıştırdığınızı test edin
          */
 
+        //1.https://html.com/tags/iframe/ sayfasına gidelim
         driver.get("https://html.com/tags/iframe/");
 
+        //2.videoyu görecek kadar aşağıya in
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN)
+                .sendKeys(Keys.PAGE_DOWN)
+                .perform();
+
+        //3.videoyu izlemek için play tuşuna basın
+        WebElement Iframe = driver.findElement(By.xpath("//iframe[@src='https://www.youtube.com/embed/owsfdh4gxyc']"));
+        driver.switchTo().frame(Iframe);
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']")).click();
+
+        //4.videoyu çalıştırdığınızı test edin
+        WebElement youTubeLinki = driver.findElement(By.xpath("//a[@class='ytp-youtube-button ytp-button yt-uix-sessionlink']"));
+
+        Assert.assertTrue(youTubeLinki.isDisplayed());
 
     }
 }
---
+++ src/test/java/day11_Faker_File/C03_FileExist.java
@@ -0,0 +1,5 @@
+package day11_Faker_File;
+
+public class C03_FileExist {
+
+}
