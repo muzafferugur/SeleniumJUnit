8a7effc1cfac40b234d813d01056267af7994f17
muzafferugur <av.muzafferugur@gmail.com>
2023-05-09 14:45:13 +0300

    09.05.2023

--- src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
+++ src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
@@ -22,6 +22,6 @@ public class C03_IstenenWebElementSS extends TestBase {
         WebElement sonucYaziElementi= driver.findElement(By.className("a-size-base s-desktop-toolbar a-text-normal"));
 
         File sonucYaziElementSS=new File("target/ekranGoruntuleri/sonucYazisiSS.jpeg");
-
+        File temp
     }
 }
