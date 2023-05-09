4a3d26a994878afb70719b9c80109f5d84aeb645
muzafferugur <av.muzafferugur@gmail.com>
2023-05-09 17:49:33 +0300

    09.05.2023

--- src/test/java/day15_WriteExcel_ScreenShot/C04_JS_Executor.java
+++ src/test/java/day15_WriteExcel_ScreenShot/C04_JS_Executor.java
@@ -7,6 +7,24 @@ import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 public class C04_JS_Executor extends TestBase {
+    /*
+    ***Js Executors***
+
+    * Selenium ile web elementlerinin kontrollerini sağlarken selenium komutları yetersiz  kalabilir veya sorunlarla karşılaşabiliriz.
+    * Bu durumlarda laternatif olarak üstesinden gelmek için JavascriptExecutor classını dahil edebiliriz.
+    * Javascript HTML kodlara direkt erişip yönetebilen bir script dili olduğundan bize çok fazla kolaylık sağlayabilir.
+
+    Peki nasıl yapılır?
+
+    *ı.adım:            JavascriptExecutor kullanmak için öncelikle driverımızı JavascriptExecutor'a cast edelim.
+    *                           JavascriptExecutor jse=(JavascriptExecutor) driver;
+    * ıı.adım:          Kullanacağımız web elementi Locate edelim.
+    *                           WebElement sellElementi= driver.findElement(By.xpath("//a[text()='Sell']"));
+    * ııı.adım:         Js driver ilw executeScript methodunu çalıştıralım,içine parameter olarak ilgili script ve webelementi yazalım.
+    *                            jse.executeScript("ilgili script", "web element");=>örneğin Sell elementine click yapmak için==> jse.executeScript("arguments[0].click();", sellElementi);
+    *
+    * Bir de ScrollIntoView özelliği var bunu da aşağıdaki soruda görebiliriz.
+     */
     @Test
     public void JSExecutorTest() throws InterruptedException {
         driver.get("https://www.amazon.com");
@@ -17,8 +35,9 @@ public class C04_JS_Executor extends TestBase {
         jse.executeScript("arguments[0].scrollIntoView(true);", signInButonu);
 
         //sign in butonuna js ile click yapalım
+        jse.executeScript("arguments[0].click();", signInButonu);
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
     }
 }
