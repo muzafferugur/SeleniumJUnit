b9f0d2616e8683d55f4cd7d134eedf9ab8af2831
muzafferugur <av.muzafferugur@gmail.com>
2023-05-09 16:11:33 +0300

    09.05.2023

--- src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
+++ src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
@@ -1,26 +1,37 @@
 package day15_WriteExcel_ScreenShot;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 import java.io.File;
+import java.io.IOException;
 
 public class C03_IstenenWebElementSS extends TestBase {
 
     @Test
-    public void webElementSS() {
+    public void webElementSS() throws IOException {
 
-       // amazona gidip nutella aratalım ve sonuç sayısının olduğu web elementin fotoğrafını çekelim
+        // amazona gidip nutella aratalım ve sonuç sayısının olduğu web elementin fotoğrafını çekelim
+
         driver.get("https://www.amazon.com");
-        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
-        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
 
-        WebElement sonucYaziElementi= driver.findElement(By.className("a-size-base s-desktop-toolbar a-text-normal"));
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
 
-        File sonucYaziElementSS=new File("target/ekranGoruntuleri/sonucYazisiSS.jpeg");
+        WebElement sonucYaziElementi = driver.findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"));//Bir WebElementin fotoğrafını çekmek istersek bu şekilde
+        //tüm sayfanın fotoğrafını çekmek isstersek =>  TakesScreenshot ts = (TakesScreenshot) driver;
+
+        File sonucYaziElementSS = new File("target/ekranGoruntuleri/sonucYazisiSS.jpeg");
+
+        File temp = sonucYaziElementi.getScreenshotAs(OutputType.FILE);
+
+        FileUtils.copyFile(temp, sonucYaziElementSS);
+
 
     }
 }
