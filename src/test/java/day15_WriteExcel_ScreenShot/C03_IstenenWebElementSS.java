cd10db239edb8050be8fc388087f42a3a33fcf40
muzafferugur <av.muzafferugur@gmail.com>
2023-05-09 14:43:27 +0300

    09.05.2023

--- src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
+++ src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
@@ -1,7 +1,12 @@
 package day15_WriteExcel_ScreenShot;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
+
+import java.io.File;
 
 public class C03_IstenenWebElementSS extends TestBase {
 
@@ -9,6 +14,14 @@ public class C03_IstenenWebElementSS extends TestBase {
     public void webElementSS() {
 
        // amazona gidip nutella aratalım ve sonuç sayısının olduğu web elementin fotoğrafını çekelim
+
+        driver.get("https://www.amazon.com");
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+
+        WebElement sonucYaziElementi= driver.findElement(By.className("a-size-base s-desktop-toolbar a-text-normal"));
+
+        File sonucYaziElementSS=new File("target/ekranGoruntuleri/sonucYazisiSS.jpeg");
 
     }
 }
